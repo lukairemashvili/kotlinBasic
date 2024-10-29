1bb764ba072dde8895734ee1d61e337a34f49d19
LevanIremashvili <levan.iremashvili16@gmail.com>
2024-10-29 22:05:07 +0400

    kotlin basic commit

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+//    val result = listOf(1,2,3).map {it*2}
+//    println(result)
+
+//    val numbers = mutableListOf(1,2,3)
+//    numbers.add(4)
+//    println(numbers)
+
+//    val  list = listOf("apple", "banana", "kiwi")
+//    val  uppercased = list.map {it.toUpperCase()}
+//    println(list)
+
+//    val  range = 1..5
+//    for (i in range step 2){
+//        println(i)
+//    }
+
+//    val set = mutableSetOf(1,2,3)
+//    val wasRemoved = set.remove(3)
+//    println(wasRemoved)
+
+}
